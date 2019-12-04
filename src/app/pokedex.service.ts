42cb44a168c71b6c1b992ecbb81bb7c978f4e2f6
Samuel R. Maggay <smaggay@sviisca.com>
2019-12-05 07:18:49 +0800

    upd.12.05.19.0718

--- src/app/pokedex.service.ts
+++ src/app/pokedex.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 // import Http service
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 // add toPromise() method
 import 'rxjs/add/operator/toPromise';
@@ -11,15 +11,27 @@ import 'rxjs/add/operator/toPromise';
 })
 export class PokedexService {
   private baseUrl: string = 'https://pokeapi.co/api/v2/pokemon/';
+  private baseSpriteUrl: string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
   
   // inject http service in constructor
-  constructor(private http: HttpClientModule) { }
+  constructor(private http: HttpClient) { }
 
   // pokemon API fetch method
   getPokemon(offset: number, limit: number) {
     return this.http.get(`${this.baseUrl}?offset=${offset}&limit=${limit}`)
+    .toPromise()
+    .then(response => response.json().results)
+    .then(items => items.map((item, idx) => {
+
+      const id: number = idx + offset + 1;
+
+      return {
+        name: item.name,
+        sprite: `${this.baseSpriteUrl}${id}.png`,
+        id
+      };
+    }));
 
     }
     
   }
-}
